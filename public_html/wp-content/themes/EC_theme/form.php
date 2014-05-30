4f7c70894769c1e48c147374d195fc87b9411835
Andre Marcelo-Tanner <andre@enthropia.com>
2014-05-30 16:45:41 +0800

    updating tracking

--- public_html/wp-content/themes/EC_theme/form.php
+++ public_html/wp-content/themes/EC_theme/form.php
@@ -11,16 +11,16 @@
 <p class="opt_in_desc">Free Video Training</p>
 
 
-<form action="https://www.mcssl.com/app/contactsave.asp" method="post" id="EmailOptIn" data-form-name="Homepage Deadlift Opt In"> <input id="merchantid" name="merchantid" type="hidden" value="84520" /> <input id="ARThankyouURL" name="ARThankyouURL" type="hidden" value="www.ericcressey.com" /> <input id="copyarresponse" name="copyarresponse" type="hidden" value="1" /> <input id="custom" name="custom" type="hidden" value="0" /> <input id="defaultar" name="defaultar" type="hidden" value="178309" /> <input id="allowmulti" name="allowmulti" type="hidden" value="0" /> <input id="visiblefields" name="visiblefields" type="hidden" value="Name,Email1" /> <input id="requiredfields" name="requiredfields" type="hidden" value="Email1" />
+<form action="https://www.mcssl.com/app/contactsave.asp" method="post" id="EmailOptIn" class="track-form" data-form-name="Homepage Deadlift Opt In" validate="validate"> <input id="merchantid" name="merchantid" type="hidden" value="84520" /> <input id="ARThankyouURL" name="ARThankyouURL" type="hidden" value="www.ericcressey.com" /> <input id="copyarresponse" name="copyarresponse" type="hidden" value="1" /> <input id="custom" name="custom" type="hidden" value="0" /> <input id="defaultar" name="defaultar" type="hidden" value="178309" /> <input id="allowmulti" name="allowmulti" type="hidden" value="0" /> <input id="visiblefields" name="visiblefields" type="hidden" value="Name,Email1" /> <input id="requiredfields" name="requiredfields" type="hidden" value="Email1" />
 <table border="0" cellspacing="2" width="205">
 <tbody>
 <tr>
 <td style="font-size:90%; color:#ffffff;" >Name:*</td>
-<td><input name="Name" type="text" /></td>
+<td><input name="Name" class="name" type="text" /></td>
 </tr>
 <tr>
 <td style="font-size:90%; color:#ffffff;">Email:*</td>
-<td><input name="Email1" type="text" /></td>
+<td><input name="Email1" class="email" type="email" required="required" /></td>
 </tr>
 <tr align="center">
 <td colspan="2"><input class="orange_button" name="cmdSubmit" type="Submit" value="Get Access Now!" /></td>
